f479e3facc52e2316bd8400b9c21ec1f22d8cc51
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-10 23:28:25 +0000

    <add>ルーティングcreate修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get'/top' => 'homes#top'
   get'books/index'
   get'books/new'
-  post'books' => 'lists#create'
+  post'books' => 'books#create'
   get'books/edit'
   get'books/show'
 end
