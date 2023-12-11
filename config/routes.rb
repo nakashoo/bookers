6ee489ccdf2358e874f8614c3bf15fbd57668d48
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-11 01:43:45 +0000

    <add>ルーティングをroot、resourcsesに書換

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-
-  get'/top' => 'homes#top'
-  get'books/index'
-  get'books/new'
-  post'books' => 'books#create'
-  get'books/edit'
-  get'books/:id' => 'books#show'
+  root "books#index"
+  resources :books
 end
