a83cda31c56bbb0feb2e7bd88bbf25e686c67104
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-11 09:26:13 +0000

    <add>editとdelete追記

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   post'books' => 'books#create'
   get'books/:id/edit' => 'books#edit',as: 'edit_book'
   get'books/:id' => 'books#show',as:'list'
+  delete'books/:id' => 'books#destroy',as:'destroy_book'
 end
