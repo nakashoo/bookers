9f3712f8c4348f0c5d9f114c382e6d826ac578ca
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-11 12:18:35 +0000

    <add>createのresirect表記変更、showとeditとdestroy追記

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,27 @@
 class BooksController < ApplicationController
 
-  def new
+  def index
+    @books = Book.all
     @book = Book.new
   end
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to'/books/:id'
+    redirect_to book_path(book.id)
   end
 
+  def show
+    @book = Book.find(params[:id])
+  end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+
+  end
 
   private
   def book_params
