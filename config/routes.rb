134ced99def3aff3f41197455725086b26ecacfc
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-11 09:02:42 +0000

    <add>show、edit修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root "books#index"
-  resources :books
+  get'homes/top' => 'homes#top'
+  get'books/index'
+  get'books/new'
+  post'books' => 'books#create'
+  get'books/:id/edit' => 'books#edit',as: 'edit_book'
+  get'books/:id' => 'books#show',as:'list'
 end
