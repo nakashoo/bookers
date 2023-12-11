e3387c9bc80ffbc79eb5cd87ced4ab65b892aa77
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-11 12:06:47 +0000

    <add>resourcesに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   get'homes/top' => 'homes#top'
-  get'books/index'
-  get'books/new'
-  post'books' => 'books#create'
-  get'books/:id/edit' => 'books#edit',as: 'edit_book'
-  get'books/:id' => 'books#show',as:'list'
-  delete'books/:id' => 'books#destroy',as:'destroy_book'
+  resources :books
 end
